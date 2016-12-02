bd7362770317520c5a0f4696f0b12731a1f46424
elzbietawatroba <elzbietawatroba@UKLOW71485>
2016-12-02 17:38:19 +0000

    Add project files.

---
+++ Ch06_PacktLibrary/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packt.CS6
+{
+    public class Person 
+        // if not public will be accessible only withing the assembly that defined it
+    {
+        // fields
+        public string Name; // if private only accessible withing the class
+        public DateTime DateOfBirth;
+
+    }
+}
---
+++ Ch06_PeopleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Packt.CS6;
+using static System.Console;
+
+namespace Ch06_PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Person();
+            p1.Name = "Misiu Jogi";
+            p1.DateOfBirth = new DateTime(1980, 12, 24);
+            WriteLine($"{p1.Name} was born on {p1.DateOfBirth: dddd, d MMMM yyyy}");
+            // curly brackets for initializing
+            var p2 = new Person { Name = "Mis Puchatek", DateOfBirth = new DateTime(1975, 12, 24) };
+            WriteLine($"{p2.Name} was born on {p2.DateOfBirth: dddd, d MMMM yyyy}");
+
+        }
+    }
+}
