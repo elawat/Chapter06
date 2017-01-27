ea705ee02a6342f4d27fb85af459095adf4415ba
elzbietawatroba <elzbietawatroba@UKLOW71485>
2017-01-27 17:27:03 +0000

    PrimeFactors

--- Ch06_Excercise/Program.cs
+++ Ch06_Excercise/Program.cs
@@ -9,9 +9,10 @@ namespace Ch06_Excercise02
 {
     class Program
     {
+        static int number;
+
         static void Main(string[] args)
         {
-            int number;
             WriteLine("Input a cardinal number");
             number = int.Parse(ReadLine());
 
@@ -21,6 +22,7 @@ namespace Ch06_Excercise02
 
             WriteLine(FactorialLoop(number));
             WriteLine(FactorialRecursion(number));
+            WriteLine(PrimeFactors(number));
         }
 
         static string CardinalToOrdinal(int cardinal)
@@ -50,13 +52,13 @@ namespace Ch06_Excercise02
             return result;
         }
 
-        static int FactorialLoop(int number)
+        static long FactorialLoop(int number)
         {
 
             if (number <= 1)
                 return 1;
 
-            int result = 1;
+            long result = 1;
             for (int i = 1; i <= number; i++)
             {
                 result = result * i;
@@ -66,12 +68,61 @@ namespace Ch06_Excercise02
 
         }
 
-        static int FactorialRecursion(int number)
+        static long FactorialRecursion(int number)
         {
             if (number <= 1)
                 return 1;
 
             return number * FactorialRecursion(number - 1);
+
+        }
+
+
+        static string PrimeFactors(int n)
+        {
+
+            if (n == 1) return ""; //The number 1 is called a unit. It has no prime factors and is neither prime nor composite.
+            if (isPrime(n)) return $"{n}";
+            bool isEqual = n % 2 == 0;
+
+            int m;
+            int i;
+            string factors;
+
+           
+                i = 2;
+                m = n / i;
+                factors ="";
+                if (isEqual) factors = ""+i;
+                while (isPrime(m)==false)
+                {
+                    for (int j =2; m%i != 0; j++) //look for devider
+                    {
+                        i = j;
+                    }
+                    m = m / i;
+                    factors = factors + i + "x";
+                }
+                factors = factors + "x" + m;
+                return factors;
+         
+      
+        }
+
+        static bool isPrime(int number)
+        {
+
+            if (number == 1) return false;
+            if (number == 2) return true;
+
+            if (number % 2 == 0) return false; //Even number     
+
+            for (int i = 3; i < number; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
 
         }
 
