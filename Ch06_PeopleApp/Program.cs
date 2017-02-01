cf3ace068bf8499df961257f9cdd681b0854420c
elzbietawatroba <elzbietawatroba@UKLOW71485>
2017-02-01 18:20:14 +0000

    Defining struct

--- Ch06_PeopleApp/Program.cs
+++ Ch06_PeopleApp/Program.cs
@@ -122,6 +122,11 @@ namespace Ch06_PeopleApp
                 WriteLine($"{person.Name}");
             }
 
+            var dv1 = new DisplacementVector(3, 5);
+            var dv2 = new DisplacementVector(-2, 7);
+            var dv3 = dv1 + dv2;
+            WriteLine($"({dv1.X}, {dv1.Y}) + ({dv2.X}, {dv2.Y}) = ({dv3.X}, {dv3.Y})");
+
         }
 
         private static void P1_Shout(object sender, EventArgs e)
